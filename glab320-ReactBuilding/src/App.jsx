91a6d6b9c1c1dce151047d5667f0c44dce695af2
PriteeTangle <140731865+priteetangle1984@users.noreply.github.com>
2024-04-17 14:02:02 -0400

    updated project

--- glab320-ReactBuilding/src/App.jsx
+++ glab320-ReactBuilding/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import "./App.css";
+const Header = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="header">
+      <h1>Simple React Application</h1>
+    </div>
+  );
+};
+const Content = ({ color, text }) => {
+  return <p style={{ color: color }}>{text}</p>;
+};
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h1>Created by Me, of course.</h1>
+    </div>
+  );
+};
+function App() {
+  return (
+    <div className="main">
+      <Header />
+      <div className="container">
+        <Content color="blue" text="This is my first React Application!" />
+        <Content color="red" text="Wish me luck..." />
+        <Content color="green" text="I think I've got it!" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Footer />
+    </div>
+  );
 }
-
-export default App
+export default App;
